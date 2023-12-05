32023bb04804141c00b748ec22eeb197870e2e15
nemoboi <148422945+nemoboi@users.noreply.github.com>
2023-12-05 18:52:23 +0100

    Add files via upload

---
+++ src/elus/commands/start.py
@@ -0,0 +1,7 @@
+import click
+
+@click.command()
+
+def start():
+    '''starts the tool background process (demon)'''
+    print('This will start up the tool later')
---
+++ src/elus/commands/stop.py
@@ -0,0 +1,7 @@
+import click
+
+@click.command()
+
+def stop():
+    '''stops the tool demon and abandons all processes'''
+    print('This will stop the tool later')
