192ed642701228c0108ed39ef9d582c200b9a025
nemoboi <nemo24751@gmail.com>
2024-01-07 15:15:46 +0100

    update areas command

---
+++ src/electricity_usage/commands/areas.py
@@ -0,0 +1,9 @@
+import click
+
+@click.command()
+def areas():
+    '''shows list of possible area codes and the corresponding area codes'''
+    #print('shows list of area codes and corresponding areas')
+    print('Currently available area Codes:\n
+    DE      Germany\n
+    BE      Belgium\n')
