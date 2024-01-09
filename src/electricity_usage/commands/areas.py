947d9a9d28c337e3173f6d0cb7c705bfefb5b5e1
nemoboi <nemo24751@gmail.com>
2024-01-09 15:44:07 +0100

    Start daemon fix, area code fix, stop edit [nemoshenanigans]

--- src/electricity_usage/commands/areas.py
+++ src/electricity_usage/commands/areas.py
@@ -4,6 +4,6 @@ import click
 def areas():
     '''shows list of possible area codes and the corresponding area codes'''
     #print('shows list of area codes and corresponding areas')
-    print('Currently available area Codes:\n
-    DE      Germany\n
-    BE      Belgium\n')
+    print('Currently available area Codes:\n',
+        'DE      Germany\n',
+        'BE      Belgium\n')
--- src/elus/commands/start.py
+++
@@ -1,7 +0,0 @@
-import click
-
-@click.command()
-
-def start():
-    '''starts the tool background process (demon)'''
-    print('This will start up the tool later')
