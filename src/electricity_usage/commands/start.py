df9537775dff605d9186bb67168c8df520496b25
nemoboi <148422945+nemoboi@users.noreply.github.com>
2024-01-11 14:55:54 +0100

    Update start.py
    minor changes (area dependency)

--- src/electricity_usage/commands/start.py
+++ src/electricity_usage/commands/start.py
@@ -9,5 +9,5 @@ area_list=['DE','BE']
 def start(area):
     '''starts the tool background process (demon)'''
     input_dir = os.path.join((os.path.dirname(os.path.abspath(__file__))),f'input_data_{area}')
-    os.system(f'python3 daemon.py {area}')
+    os.system(f'python3 daemon.py')# {area}')
 
