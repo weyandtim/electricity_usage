cd5612887a579084a5eb2def508990d43457a97d
nemoboi <nemo24751@gmail.com>
2024-02-26 10:56:28 +0100

    wip start command

--- electricity_usage/daemon.py
+++ electricity_usage/daemon.py
@@ -4,6 +4,7 @@ import time
 import os
 import sys
 import subprocess
+import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from electricity_usage import job
@@ -22,6 +23,7 @@ class Daemon:
             self.stop_event = threading.Event()  # Event für das Beenden des Daemon-Threads
             self.em_API_key = em_API_key
             self.area = area
+            self.input_dir = input_dir
 
             # Watchdog Setup
             self.event_handler = CustomFileSystemEventHandler(self)
@@ -35,7 +37,7 @@ class Daemon:
             print("Daemon is running...")
             
             power_production, power_consumption = em_data.get_power_data(self.area, self.em_API_key)
-            #print(power_production, power_consumption)
+            print(power_production, power_consumption)
 
             # check conditions
             if power_production is not None and power_consumption is not None:
@@ -43,8 +45,9 @@ class Daemon:
                     # run processes
                     with self.lock:
                         for job_instance in self.jobs:
-                            deadline = datetime.strptime(job_instance.deadline,"%Y-%m-%d %H:&M:%S")
-                            if deadline <= datetime.now():
+                            deadline = datetime.datetime.strptime(job_instance.deadline,"%Y-%m-%d %H:%M:%S")
+                            # check deadline
+                            if deadline <= datetime.datetime.now():
                                 commandline = job_instance.commandline
                                 job_id = job_instance.job_id
                                 print(f"Executing commandline for Job {job_id}: {commandline}")
@@ -53,7 +56,7 @@ class Daemon:
                                 subprocess.run(commandline, shell=True, check=True)
             
             # time.sleep(900)
-            time.sleep(2)
+            time.sleep(10)
 
         print("Daemon is terminating...")
         self.observer.stop()
@@ -62,6 +65,24 @@ class Daemon:
 
     def stop(self):
         self.stop_event.set()  # Setzen Sie das Event, um den Daemon-Thread zu beenden
+        try:
+            # Überprüfen, ob der Ordner existiert
+            if os.path.exists(self.input_dir):
+                # Liste aller Dateien im Ordner
+                files = os.listdir(self.input_dir)
+                for file_name in files:
+                    # Pfad zur Datei erstellen
+                    file_path = os.path.join(self.input_dir, file_name)
+                    # Überprüfen, ob das Element ein reguläres File ist
+                    if os.path.isfile(file_path):
+                        # Datei löschen
+                        os.remove(file_path)
+                print("Alle Dateien im Ordner wurden erfolgreich gelöscht.")
+            else:
+                print(f"Der Ordner {self.input_dir} existiert nicht.")
+        except Exception as e:
+            print(f"Fehler beim Löschen der Dateien: {str(e)}")
+
 
 
     def process_json_file(self, file_path):
@@ -94,13 +115,22 @@ class CustomFileSystemEventHandler(FileSystemEventHandler):
 
     def on_created(self, event):
         if event.is_directory:
+            print("event erkannt")
             return
-        elif event.event_type == 'created' and event.src_path.endswith('.json'):
-            print(f"New JSON file detected: {event.src_path}")
-            self.daemon_instance.process_json_file(event.src_path) #ruft die Methode process_json_file des Daemons auf
-                
+        elif event.event_type == 'created':
+            if event.src_path.endswith('.json'):#ruft die Methode process_json_file des Daemons auf
+                print(f"New JSON file detected: {event.src_path}")
+                self.daemon_instance.process_json_file(event.src_path)     
+            elif event.src_path.endswith('txt'):#ruft die Methode stop des Daemons auf
+                print("Stop token file detected. Stopping daemon.")
+                self.daemon_instance.stop() 
 
 
+
+'''
+elif event.src_path.startswith('stop_token'):#ruft die Methode stop des Daemons auf
+                print("Stop token file detected. Stopping daemon.")
+                self.daemon_instance.stop() ''' 
 
 if __name__ == "__main__":
 
@@ -112,25 +142,15 @@ if __name__ == "__main__":
     os.makedirs(input_dir, exist_ok=True)
 
     # Instanziieren Sie den Daemon
-    daemon = Daemon(os.getenv("API_KEY"), area, input_dir)
+    daemon = Daemon(os.getenv("API_KEY"), 'DE', 'C:\git\electricity_usage\input_data')
 
     # Starten Sie die run-Methode des Daemons in einem separaten Thread
-    daemon_thread = threading.Thread(target=daemon.run, daemon=True)
+    daemon_thread = threading.Thread(target=daemon.run) #daemon=True darf nicht gesetzt werden
     daemon_thread.start()
 
     if daemon.observer.is_alive():
         print("Observer aktiv")
 
-    # Erstellen Sie einige Beispiel-JSON-Dateien im input_data-Ordner
-    json_data_1 = { "estimate": 100, "deadline": "2023-12-31", "commandline": 'echo "test erfolgreich"'}
-    json_data_2 = { "estimate": 200, "deadline": "2023-12-31", "commandline": 'echo "test 2 erfolgreich"'}
+    time.sleep(15)
 
-    with open("input_data/data1.json", "w") as file:
-        json.dump(json_data_1, file)
-
-    with open("input_data/data2.json", "w") as file:
-        json.dump(json_data_2, file)
-
-    time.sleep(6)
-
-    daemon.stop()
+    #daemon.stop()
