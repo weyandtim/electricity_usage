18d713bdd7e9abdf55dbd68e3c4918b540dca6e1
nemoboi <nemo24751@gmail.com>
2024-03-26 21:07:20 +0100

    meta cleanup

--- electricity_usage/data_dirs.py
+++ electricity_usage/data_dirs.py
@@ -21,27 +21,6 @@ def create_input_dir_path():
         os.makedirs(input_dir)
     return input_dir
 
-# Input directory folder for multiple Daemons (named after area)
-'''def create_input_dir_path(area):
-    # Get directory for application data
-    data_dir = platformdirs.user_data_dir(appname='electricity_usage')
-
-    # Create electricity_usage directory if it doesn't exist
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-
-    # Create input_dirs directory if it doesn't exist
-    input_dirs_dir = os.path.join(data_dir, 'input_dirs')
-    if not os.path.exists(input_dirs_dir):
-        os.makedirs(input_dirs_dir)
-
-    # Create input_dir in the input_dirs directory if it doesn't exist
-    input_dir = os.path.join(input_dirs_dir, f'input_dir_{area}')
-    if not os.path.exists(input_dir):
-        os.makedirs(input_dir)
-
-    return input_dir'''
-
 # Returns the path to the input_dir folder
 def get_input_dir_path():
     # Get directory for application data
