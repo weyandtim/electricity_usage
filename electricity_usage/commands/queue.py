190253bf5664106c3a15084e8b4be47907c46a9c
nemoboi <nemo24751@gmail.com>
2024-03-27 16:45:32 +0100

    wip for proper unittests

--- electricity_usage/commands/queue.py
+++ electricity_usage/commands/queue.py
@@ -1,22 +1,18 @@
 import click
 import os
+from ..data_dirs import get_input_dir_path
 
 @click.command()
 
 def queue():
     '''shows a list of processes in queue'''
-    here = os.path.dirname(os.path.abspath(__file__))
-    main = os.path.join(here, '../input_data')
-    dirs = os.listdir(main)
-    for d in dirs:
-        print('For Subdirectory '+d+':\n')
-        path_to_dir = os.path.join(main,d)
-        files = [f for f in listdir(path_to_dir) if isfile(join(path_to_dir, f))]
-        for f in files:
-            #with open(f) as j:
-            #    c = j.commandline
-            #    print(f, c)
-            print(f)
+    path_to_dir = get_input_dir_path()
+    print(f"In your input directory {path_to_dir}, you currently have the following jobs in your queue:\n")
+    files = [f for f in path_to_dir if os.path.isfile(os.path.join(path_to_dir, f))]
+    for f in files:
+        #with open(f) as j:
+        #    c = j.commandline
+        #    print(f, c)
+        print(f)
         print("\n")
-
     
---
+++ tests/test_cli_.py
@@ -0,0 +1,64 @@
+import pytest
+from unittest.mock import patch, MagicMock
+import platformdirs
+import os
+import time
+from click.testing import CliRunner
+from electricity_usage.__main__ import cli
+from electricity_usage.commands.area_codes import areas_str
+from electricity_usage.data_dirs import get_input_dir_path
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+@pytest.fixture
+def data_dir():
+    data_dir = platformdirs.user_data_dir(appname='electricity_usage')
+    return data_dir
+
+######  areas  ######
+def test_areas_command(runner):
+    longstring = "ElectricityMaps offers data from areas corresponding to the following areas\n"
+    for a in areas_str:
+        longstring = longstring + f"\n{a}"
+    longstring = longstring + "\n"
+    result = runner.invoke(cli, ['areas'])
+    assert result.exit_code == 0
+    assert result.output == longstring
+
+
+######  queue  ######
+def test_queue_command(runner):
+    result = runner.invoke(cli, ['queue'])
+    test_path = get_input_dir_path()
+    assert result.exit_code == 0
+    assert test_path in result.output
+
+
+######  start  ######
+def test_start_command_default(runner):
+    result = runner.invoke(cli, ['start'])
+    assert result.exit_code == 0
+
+def test_start_command_with_valid_area(runner):
+    result = runner.invoke(cli, ['start', '--area', 'BE'])
+    assert result.exit_code == 0
+
+def test_start_command_with_invalid_area(runner):
+    result = runner.invoke(cli, ['start', '--area', 'FF'])
+    assert result.exit_code == 2
+
+
+######  run  ######
+def test_run_command_valid(runner, data_dir):
+    result = runner.invoke(cli, ['run', '--estimate 1 --deadline "2025-02-22" --commandline "command"'])
+    #assert result.exit_code == 0
+    assert os.path.isdir(os.path.join(data_dir, 'input_dirs/input_data')) == True
+
+######  stop  ######
+def test_stop_command_valid(runner, data_dir):
+    result = runner.invoke(cli, ['stop'])
+    assert result.exit_code == 0
+    time.sleep(2)  #wait for stop command to be done deleting directories
+    assert os.path.isdir(data_dir)==False
